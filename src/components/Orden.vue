22f2005c1e1d055067d02bf126165b865c27105d
Marc Obel <mao@syseng.dk>
2024-11-24 11:49:26 +0100

    initial commit

---
+++ src/components/Orden.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { onKeyStroke } from '@vueuse/core';
+
+const content = ref('');
+
+onKeyStroke((event) => {
+  if (event.key === 'Backspace') {
+    content.value = content.value.slice(0, -1);
+  } else if (event.key === 'Meta' || event.key === 'CapsLock') return
+  else {
+    content.value += event.key;
+  }
+});</script>
+
+<template>
+  <h1 class="rainbow">{{ content }}</h1>
+</template>
+
+<style scoped>
+.rainbow {
+  background-image: linear-gradient(to right,
+      #d20f39, #fe640b, #df8e1d, #40a02b, #04a5e5, #ea76cb, #8839ef);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-optical-sizing: auto;
+  font-style: normal;
+  overflow-wrap: anywhere; /* Break words wherever needed */
+}
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <Orden />
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
